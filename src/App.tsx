0f9309cafd970afb2bb7273113cd37cddcbc10b8
Sergey Obmolov <winchestersoro@gmail.com>
2024-12-15 17:41:30 +0300

    pre-merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import MainBlock from "./components/MainBlock/MainBlock";
 import NotificationBlock from "./components/NotificationBlock/NotificationBlock";
+import mockData from "./MokDate/mockData"
 
+ 
 function App() {
  return (
+    
   <section className="UserView">
    <MainBlock />
    <NotificationBlock />
   </section>
+   
  );
 }
 
